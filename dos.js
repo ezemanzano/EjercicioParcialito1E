44503e9b5031d82462e03caf2d33ed99877ec954
ezemanzano <ezequielm789@gmail.com>
2020-08-05 22:22:20 -0300

    parcial 2

--- dos.js
+++ dos.js
@@ -10,7 +10,55 @@ Al finalizar, se debe informar:
 Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
 */
 
-function mostrar()
-{
-	alert("Hola Mundo");
+function mostrar() {
+	let vNombre;
+	let vCate;
+	let vPrecio;
+	let vMin;
+	let vMax;
+	let respuesta;
+	let vNombreLac;
+	let vPrecioLac;
+	let vPrecioLac2;
+	let vCantidadBedidas = 0;
+	let cont = 0;
+	respuesta = "s";
+
+	do {
+
+		vNombre = prompt("Ingrese nombre :");
+		do {
+			vCate = prompt("Ingrese categoría :");
+		}
+		while (!(vCate == "almacen" || vCate == "lácteos" || vCate == "limpieza" || vCate == "bebidas"))
+		do {
+			vPrecio = parseInt(prompt("Ingrese precio :"));
+		} while (!(vPrecio > 0 && vPrecio < 1000));
+		respuesta = prompt("desea continuar? s/n")
+
+		if (vCate == "lácteos"){
+		if (cont == 1) {
+			vPrecioLac = vPrecio;
+			vPrecioLac2 = vPrecio;
+		}
+		cont++
+		if (vPrecio < vPrecioLac) {
+			vPrecioLac = vPrecio;
+		} else if (vPrecio > vPrecioLac2) {
+			vPrecioLac2 = vPrecio;
+			vNombreLac = vNombre;
+		}
+		}
+
+
+		if (vCate == "bebidas") {
+			vCantidadBedidas++;
+		}
+	}
+
+	while (respuesta == "s");
+	
+	alert ("El Lácteo con mayor precio es " + vNombre +" " + vPrecioLac2);
+	alert ("La cantidad de articulos en bebidas es: " + vCantidadBedidas);
+	
 }
